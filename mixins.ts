68afab8b769953cdf78a56dcd662e4a517c06432
Kristján Oddsson <koddsson@gmail.com>
2024-02-29 08:23:13 +0100

    Work on mixin some more

--- mixins.ts
+++ mixins.ts
@@ -1,8 +1,9 @@
 import { ObservableMap } from "./context-protocol.js";
-import { createContext, ContextEvent } from "./index.js";
+import { createContext, ContextEvent, UnknownContext } from "./index.js";
 
 interface CustomElement extends HTMLElement {
   new (...args: any[]): CustomElement;
+  observedAttributes: string[];
   connectedCallback?(): void;
   attributeChangedCallback?(
     name: string,
@@ -19,51 +20,62 @@ export function ProviderMixin(Class: CustomElement) {
 
     connectedCallback() {
       super.connectedCallback?.();
-     
+
       for (const attribute in this.observedAttributes) {
         this.#dataStore.set(attribute, this.getAttribute(attribute));
       }
 
-      // We listen for a bubbled context request event and provide the event with the context requested.
-      this.addEventListener(
-        "context-request",
-        (event: ContextEvent<{ name: string; initialValue?: unknown }>) => {
-          const { name, initialValue } = event.context;
-          const subscribe = event.subscribe;
-          if (initialValue) {
-            this.#dataStore.set(name, initialValue);
-          }
-          const data = this.#dataStore.get(name);
-          if (data) {
-            event.stopPropagation();
-
-            let unsubscribe: () => void | undefined;
-
-            if (subscribe) {
-              unsubscribe = () => {
-                data.subscribers.delete(event.callback);
-              };
-              data.subscribers.add(event.callback);
-            }
-
-            event.callback(data.value, unsubscribe);
-          }
-        },
-      );
+      this.addEventListener('context-request', this);
     }
 
+    // Listen for changed attributes and update the data store accordingly
     attributeChangedCallback(name: string, oldValue: string, newValue: string) {
       super.attributeChangedCallback?.(name, oldValue, newValue);
       this.#dataStore.set(name, newValue);
     }
 
-    // TODO: Something in the disconnectedCallback??
+    disconnectedCallback(): void {
+      this.#dataStore = new ObservableMap();
+    }
+
+    handleEvent(event: Event) {
+      if (event.type === "context-request") {
+        this.#handleContextRequest(event as ContextEvent<UnknownContext>);
+      }
+    }
+
+    // We listen for a bubbled context request event and provide the event with the context requested.
+    #handleContextRequest(
+      event: ContextEvent<{ name: string; initialValue?: unknown }>,
+    ) {
+      const { name, initialValue } = event.context;
+      const subscribe = event.subscribe;
+      if (initialValue) {
+        this.#dataStore.set(name, initialValue);
+      }
+      const data = this.#dataStore.get(name);
+      if (data) {
+        event.stopPropagation();
+
+        let unsubscribe = () => undefined;
+
+        if (subscribe) {
+          unsubscribe = () => {
+            data.subscribers.delete(event.callback);
+          };
+          data.subscribers.add(event.callback);
+        }
+
+        event.callback(data.value, unsubscribe);
+      }
+    }
   };
 }
 
 export function ConsumerMixin(Class: CustomElement) {
   return class extends Class {
-    unsubscribes: Array<() => void> = []
+    unsubscribes: Array<() => void> = [];
+    contexts: Record<string, (data: unknown) => void> = {};
 
     connectedCallback() {
       super.connectedCallback?.();
@@ -83,16 +95,20 @@ export function ConsumerMixin(Class: CustomElement) {
                 this.unsubscribes.push(unsubscribe);
               }
             },
+            // Always subscribe. Consumers can ignore updates if they'd like.
             true,
           ),
         );
       }
     }
 
+    // Unsubscribe from all callbacks when disconnecting
     disconnectedCallback() {
       for (const unsubscribe of this.unsubscribes) {
         unsubscribe?.();
       }
+      // Empty out the array in case this element is still stored in memory but just not connected
+      // to the DOM.
       this.unsubscribes = [];
     }
   };
