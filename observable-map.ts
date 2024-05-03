7e739fd60714aa9b2f9eb78c99fce751c568f53f
Kristján Oddsson <koddsson@gmail.com>
2024-05-03 16:21:16 +0200

    update after interface changes

--- observable-map.ts
+++ observable-map.ts
@@ -1,19 +1,22 @@
 type Subscriber<T> = (value: T) => void;
 
-export class ObservableMap {
-  #store = new Map<string, {value: unknown, subscribers: Set<Subscriber<unknown>>}>
+export class ObservableMap<K, V> {
+  #store = new Map<K, { value: V; subscribers: Set<Subscriber<V>> }>();
 
-  set(key: string, value: unknown, subscribers = new Set<Subscriber<unknown>>()) {
+  set(key: K, value: V, subscribers = new Set<Subscriber<V>>()) {
     const data = this.#store.get(key);
-    subscribers = new Set([...subscribers, ...(data?.subscribers || new Set())]);
+    subscribers = new Set([
+      ...subscribers,
+      ...(data?.subscribers || new Set()),
+    ]);
 
-    this.#store.set(key, {value, subscribers});
+    this.#store.set(key, { value, subscribers });
     for (const subscriber of subscribers) {
       subscriber(value);
     }
   }
 
-  get(key: string) {
+  get(key: K) {
     return this.#store.get(key);
   }
 }
